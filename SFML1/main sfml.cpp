4146cd095c8dbab8ff96a4cb2d0469bec0917dcb
HoopeMax <nadezhda-maksimova-01@mail.ru>
2020-10-05 02:57:52 +0300

    Закончила динамич вариант

--- SFML1/main sfml.cpp
+++ SFML1/main sfml.cpp
@@ -2,54 +2,70 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include<iostream>
+
 //размер окна
 const float width = 1000; //ширина 
 const float height = 600; //длина 
+
 class Point_Figure
 {
-public://точка расположение в центре экрана 
+public:
+    //координаты 
     float a = width / 2.7;
     float b = height / 1.9;
-    float a1 = width / 2;
-    float b1 = height / 1.2;
+
+    //размеры 
+    int rectH = (2);
+    int rectW = (100);
+    int rectB = (50);
 };
 class Line_Figure: public Point_Figure
 {
 public:
- //размер фигуры от заданной точки 
-int rectH = (1);
-int rectW = (100);
-int rectB = (50);
+    sf::RectangleShape figure, figure1, figure2, figure3;//линии
 
-}; 
-class Object:public Line_Figure 
-{
-public:
-    sf::RectangleShape figure, figure1, figure2, figure3;
-    //Позиция и размер фигуры
-    Object()
+    Line_Figure()
     {
         sf::RectangleShape object(sf::Vector2f(rectW, rectH));
         sf::RectangleShape object1(sf::Vector2f(rectW, rectH));
         sf::RectangleShape object2(sf::Vector2f(rectH, rectB));
         sf::RectangleShape object3(sf::Vector2f(rectH, rectB));
+
         object.setFillColor(sf::Color(255, 0, 0));
         object1.setFillColor(sf::Color(255, 0, 0));
         object2.setFillColor(sf::Color(255, 0, 0));
         object3.setFillColor(sf::Color(255, 0, 0));
+
         figure = object;
         figure1 = object1;
         figure2 = object2;
         figure3 = object3;
-        float x, y, speed;
-        bool flag(true);
+    }
+
+
+}; 
+class Object:public Line_Figure 
+{
+public:
+
+    sf::RectangleShape mainfigure;
+    float x=50, y=0, speed=0;
+    bool flag;
+ 
+    Object()
+    {
+  
+        sf::RectangleShape mainobject(sf::Vector2f(rectW-2, rectB-2));
+
+        mainobject.setFillColor(sf::Color(0, 0, 250));
+
+        mainfigure = mainobject;
+        
     }
     //Движение объекта в центре экрана по синусоиде
     void moving(float t)
     {
-        x = 50;
-        y = 0;
-        speed = 0;
+      
         if (a + 450 <= width && flag)//ограниечение справа
         {
             speed = t;
@@ -67,8 +83,12 @@ public:
         }
         else flag = true;
         speed = 0;
-        figure.setPosition(a, b);
 
+        figure.setPosition(a, b);
+        figure1.setPosition(a, b+50); 
+        figure2.setPosition(a+100, b);
+        figure3.setPosition(a, b);
+        mainfigure.setPosition(a+2, b+2);
     }
    
 };
@@ -120,24 +140,39 @@ void input()
         float time = clock.getElapsedTime().asSeconds();
         clock.restart();
         sf::Event event;
+
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        moving(time);
+
+
+        lastRect->moving(time);
         poi->array_point();
         /* lastRect.moving(time);
          poi.array_point();*/
+
+
         window.clear(sf::Color(0, 0, 0));
         for (int i = 1290; i < 2340; i++)
         {
             window.draw(poi->point);
             //window.draw(poi.point);
         }
+
+      //рисуются объекты
         window.draw(lastRect->figure);
+        window.draw(lastRect->figure1);
+        window.draw(lastRect->figure2);
+        window.draw(lastRect->figure3);
+        window.draw(lastRect->mainfigure);
    
         //window.draw(lastRect.figure);
+        //window.draw(lastRect.figure1);
+        //window.draw(lastRect.figure2);
+        //window.draw(lastRect.figure3);
+
         window.display();
 
     }
